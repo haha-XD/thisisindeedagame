417d15d4c43e6a2f5c7a0bb92b31e791f11a5396
haha-XD <perfectionofthehighestorder@gmail.com>
2021-12-13 18:31:40 +1100

    pls

--- index.js
+++ index.js
@@ -19,17 +19,20 @@ var Entity = function() {
 }
 
 function applyInput(inputs, timestamp, entity) {
+	now_ts = new Date();
+	latency = now_ts - timestamp;
+
 	if (inputs[87]) {
-		entity.y -= inputs[87] * entity.speed;
+		entity.y -= (inputs[87] + (latency * 2)) * entity.speed;
 	}
 	if (inputs[83]) {
-		entity.y += inputs[83] * entity.speed;
+		entity.y += (inputs[83] + (latency * 2)) * entity.speed;
 	}
 	if (inputs[68]) {
-		entity.x += inputs[68] * entity.speed;
+		entity.x += (inputs[68] + (latency * 2)) * entity.speed;
 	}
 	if (inputs[65]) {
-		entity.x -= inputs[65] * entity.speed;
+		entity.x -= (inputs[65] + (latency * 2)) * entity.speed;
 	}
 }
 
--- public/modules/game.js
+++ public/modules/game.js
@@ -60,7 +60,7 @@ Game.prototype.performServerReconciliation = function() {
             if(this.pending_input_states.inputs) {
                 for (input of this.pending_input_states.inputs) {
                     console.log(input)
-                    applyInput(input, entity);            
+                    //applyInput(input, entity);            
                 }
                 //console.log(`new:x${entity.x}y${entity. y}`)
             }
