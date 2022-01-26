58efefcf007c6516a1d8dbbf49c18094bce59086
haha-XD <perfectionofthehighestorder@gmail.com>
2022-01-26 21:00:56 +1100

    test

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ io.on('connection', (socket) => {
 		for (let i = 0; i < 37; i++) {
 			svEntities.push(new entityTypes.Bullet(100, 100, 3, 16, i * 10));			
 		}
+		for (let i = 0; i < 37; i++) {
+			svEntities.push(new entityTypes.Bullet(100, 100, 3, 16, 5 + i * 10));			
+		}
+		for (let i = 0; i < 37; i++) {
+			svEntities.push(new entityTypes.Bullet(100, 100, 3, 16, i * 10));			
+		}
 	})
 
 	setInterval(() => {
