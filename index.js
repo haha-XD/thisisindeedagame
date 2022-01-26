a70e212456001e689dc6379042020b6f5bffa85e
haha-XD <perfectionofthehighestorder@gmail.com>
2022-01-26 19:32:36 +1100

    test

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ server.listen(port, () => {
 		for (let i = 0; i < 37; i++) {
 			svEntities.push(new entityTypes.Bullet(100, 100, 3, 16, i * 10));			
 		}
-	}, 1000)
+	}, 1000/2)
 	setInterval(update, 1000/10);
 });
