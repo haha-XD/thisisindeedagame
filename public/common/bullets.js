d537372be5844f685ab3e2ffc8ba8b87f9d6ee8f
haha-XD <perfectionofthehighestorder@gmail.com>
2022-01-27 19:57:27 +1100

    test

---
+++ public/common/bullets.js
@@ -0,0 +1,23 @@
+import { Bullet } from "./entityTypes.js";
+
+export let radialShotgun = function(x, y, spd, size, shotCount) {
+    this.creationTS = new Date().getTime()
+    this.x = x;
+    this.y = y;
+    this.speed = spd;
+    this.size = size;
+    this.shotCount = shotCount;
+
+    this.patternType = 'radial';
+}
+
+export function parsePattern(pattern, entities) {
+    if (pattern.patternType == 'radial') {
+        for(let i = 0; i < pattern.shotCount; i++) {
+            console.log('xd')
+            let bullet = new Bullet(pattern.x, pattern.y, pattern.speed, pattern.size, i * (360/pattern.shotCount))
+            bullet.creationTS = pattern.creationTS;
+            entities.push(bullet);
+        }
+    }
+}
