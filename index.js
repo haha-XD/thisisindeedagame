f70e0c294abf65f5c06d050b5c788aec52c43e55
haha-XD <perfectionofthehighestorder@gmail.com>
2022-01-28 20:37:20 +1100

    test

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ server.listen(port, () => {
 	console.log(`[SERVER] now listening to port ${port}`);
 	setInterval(update, 1000/10);
 	setInterval(() => {
-		spawnBullet(new bulletPattern.radialShotgun(100, 100, 2, 64, 3, 10));
-	}, 1000/4)
+		spawnBullet(new bulletPattern.radialShotgun(100, 100, 2, 4, 3, 72));
+	}, 1000*2)
 });
